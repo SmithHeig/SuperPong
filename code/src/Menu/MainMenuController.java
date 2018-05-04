1ef2b978968b1d42a110489b8c345431aff6381a
Unknown <james.smith@heig-vd.ch>
2018-05-04 14:39:43 +0200

    add all fxml

--- code/src/Menu/MainMenuController.java
+++ code/src/Menu/MainMenuController.java
@@ -3,8 +3,12 @@ package Menu;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 public class MainMenuController {
+
+    @FXML
+    private AnchorPane mainMenu;
 
     @FXML
     private Button LocalMenuButton;
