5c5ee6c555a9c1f71f53a340c02641063d904dac
Unknown <james.smith@heig-vd.ch>
2018-05-04 11:41:24 +0200

    temp

---
+++ code/src/Menu/MainMenuController.java
@@ -0,0 +1,26 @@
+package Menu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuController {
+
+    @FXML
+    private Button LocalMenuButton;
+
+    @FXML
+    private Button NetworkMenuButton;
+
+    @FXML
+    void goToLocalMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToNetworkMenu(ActionEvent event) {
+
+    }
+
+}
+
