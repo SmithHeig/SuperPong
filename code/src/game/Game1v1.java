5fa481400e6d3289e38d61747267b0e81c4b81e1
Unknown <ant.rochat@hotmail.fr>
2018-05-04 10:09:04 +0200

    ajout des rebonds pour le joueur ET l'IA, et amélioration de l'intelligence de l'IA

--- code/src/game/Game1v1.java
+++ code/src/game/Game1v1.java
@@ -16,8 +16,8 @@ public class Game1v1 extends Application {
 	private Ball ball;
 	private Raquet playerRaquet;
 	private Raquet bot;
-	private final int  PLAYER_RAQUET_SIZE = 200, BOT_RAQUET_SIZE = 100;
-	private int speedX = 4, speedY = 4, dv = speedX, dy = speedY;
+	private final int  PLAYER_RAQUET_SIZE = 100, BOT_RAQUET_SIZE = 100, VELOCITY = 7;
+	private double speedX = -5, speedY = 0, dv = speedX, dy = speedY;
 	
 	private final int WIDTH = 1000, HEIGHT = 600;
 	
@@ -69,10 +69,39 @@ public class Game1v1 extends Application {
 		double x = ball.getPositionX();
 		double y = ball.getPositionY();
 		
-		if (x <= 10 && y > playerRaquet.getPositionY() && y < playerRaquet.getPositionY() + playerRaquet.getHeight()) dv = speedX;
+		double ANGLE_MIN = -45;
+		double ANGLE_MAX = 45;
+		double ANGLE_DELTA = ANGLE_MAX - ANGLE_MIN;
 		
+		// si le joueur touche la balle
+		if (x <= 15 && y > playerRaquet.getPositionY() && y < playerRaquet.getPositionY() + playerRaquet.getHeight()){
+			
+			double perCent = (y - playerRaquet.getPositionY()) / playerRaquet.getHeight();
+			
+			double alpha = ANGLE_DELTA * perCent + ANGLE_MIN;
+			
+			//System.out.println(alpha);
+			
+			double rad = Math.toRadians(alpha);
+			
+			dv = VELOCITY*Math.cos(rad);
+			dy = VELOCITY*Math.sin(rad);
+		}
+		
+		ANGLE_MIN = -135;
+		ANGLE_MAX = 135;
+		ANGLE_DELTA =90;
 		if (x >= WIDTH - 10 && y > bot.getPositionY() && y < bot.getPositionY() + bot.getHeight()) {
-			dv = -speedX;
+			double perCent = (y - bot.getPositionY()) / bot.getHeight();
+			
+			double alpha = -ANGLE_DELTA * perCent + ANGLE_MIN;
+			
+			System.out.println(alpha);
+			
+			double rad = Math.toRadians(alpha);
+			
+			dv = VELOCITY*Math.cos(rad);
+			dy = VELOCITY*Math.sin(rad);
 		}
 		
 		if (x < 0){
@@ -88,22 +117,23 @@ public class Game1v1 extends Application {
 		}
 		
 		
-		if (y <= 0) dy = speedY;
-		if (y >= HEIGHT - 5) dy = -speedY;
+		if (y <= 0) dy *= -1;
+		if (y >= HEIGHT - 5) dy *= -1;
 		
 		
 		ball.setPositionX(ball.getPositionX() + dv);
 		ball.setPositionY(ball.getPositionY() + dy);
 		
+		
 		// l'ia du bot, suit la hauteur de la balle quand elle est dans sa moitié de terrain
-		if (x > WIDTH / 2 && bot.getPositionY()-bot.getHeight()/2-1 > y) {
+		if (x > WIDTH/2 &&bot.getPositionY() > y) {
 			if (bot.getPositionY() > 0) {
-				bot.setPositionY(bot.getPositionY() - 30);
+				bot.setPositionY(bot.getPositionY() - 7);
 			}
 		}
-		if (x > WIDTH / 2 && bot.getPositionY() + bot.getHeight()/2+1 < y) {
+		if (x> WIDTH/2 &&bot.getPositionY() + bot.getHeight()/2 < y) {
 			if (bot.getPositionY() + bot.getHeight() < HEIGHT) {
-				bot.setPositionY(bot.getPositionY() + 30);
+				bot.setPositionY(bot.getPositionY() + 7);
 			}
 			
 		}
