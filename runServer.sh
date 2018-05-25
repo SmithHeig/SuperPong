7211c5501d901b086638e466958721b558b1f191
Unknown <james.smith@heig-vd.ch>
2018-05-25 10:13:50 +0200

    Script update

--- runServer.sh
+++ runServer.sh
@@ -1,3 +1,11 @@
+# Build jar Lib
+mvn clean install -f SuperPong/Lib/
+
+# Build jar Server
+mvn clean install -f SuperPong/Server/
+
+# Build jar Client
+mvn clean install -f SuperPong/Client/
 echo "copie du jar du server"
 cp ./SuperPong/Server/target/Server-1.0-SNAPSHOT.jar ./Docker/server-image/src/Server-1.0-SNAPSHOT.jar
 
@@ -6,3 +14,6 @@ docker build -t superpong/server ./Docker/server-image/
 
 echo "run de l'image"
 docker run -p 666:666 superpong/server
+
+# A décommenter si docker run -d
+#java -jar ./SuperPong/Client/target/Client-1.0-SNAPSHOT.jar
