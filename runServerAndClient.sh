b356b141bc85304ec9dca799e69738020524b327
Benoît <ben17_1@hotmail.com>
2018-06-14 23:12:57 +0200

    testsMaven

--- runServerAndClient.sh
+++ runServerAndClient.sh
@@ -1,26 +1,25 @@
-docker kill server-superpong
+docker kill $(docker ps -q -a)
+docker rm $(docker ps -a -q)
 
-# Build jar Lib
-echo "building lib mvn"
-mvn clean install -f SuperPong/Lib/pom.xml
-
-# Build jar Server
-echo "building Server mvn"
-mvn clean install -f SuperPong/Server/pom.xml
-
-# Build jar Client
-echo "building Client mvn"
-mvn clean install -f SuperPong/Client/pom.xml
+#Build project and childrens
+echo "building project"
+mvn clean install -f SuperPong/pom.xml
 
 echo "copie du jar du server"
-mkdir Docker/server-image/src
-cp ./SuperPong/Server/target/Server-1.0-SNAPSHOT.jar ./Docker/server-image/src/Server-1.0-SNAPSHOT.jar
+mkdir Docker/images/src
+cp ./SuperPong/Server/target/Server-1.0-SNAPSHOT.jar ./Docker/images/src/Server-1.0-SNAPSHOT.jar
+
+echo "copie du jar de la lib"
+cp ./SuperPong/Lib/target/lib-1.0-SNAPSHOT.jar ./Docker/images/src/lib-1.0-SNAPSHOT.jar
+
+echo "copie du jar du client"
+cp ./SuperPong/Client/target/Client-1.0-SNAPSHOT.jar ./Docker/images/src/Client-1.0-SNAPSHOT.jar
 
 echo "built de l'image server"
 docker build -t superpong/server ./Docker/server-image/
 
 echo "run de l'image"
-docker run --name server-superpong -p 6666:6666 superpong/server
+docker run --name server-superpong -d -p 6666:6666 superpong/server
 
 # A décommenter si docker run -d
-#java -jar ./SuperPong/Client/target/Client-1.0-SNAPSHOT.jar
+java -jar ./SuperPong/Client/target/Client-1.0-SNAPSHOT.jar
