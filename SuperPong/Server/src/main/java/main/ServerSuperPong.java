d183d167f0287699abef65b3377508a1783818f7
Benoît <ben17_1@hotmail.com>
2018-06-15 02:22:09 +0200

    add fxml for server

--- SuperPong/Server/src/main/java/main/ServerSuperPong.java
+++ SuperPong/Server/src/main/java/main/ServerSuperPong.java
@@ -1,6 +1,12 @@
 package main;
 
-import db.DB;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import server.Server;
 
 import java.io.IOException;
@@ -8,11 +14,35 @@ import java.io.IOException;
 /**
  * Classe de lancement du serveur
  */
-public class ServerSuperPong {
-
-    /** Constructeur **/
-    public static void main(String[] argv) throws IOException{
-        Server server = new Server();
-        server.start();
-    }
+public class ServerSuperPong extends Application {
+	
+	public Button exitButton;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			FXMLLoader fxmlLoaderMainView = new FXMLLoader(getClass().getResource("/fxml/mainView.fxml"));
+			Parent root = fxmlLoaderMainView.load();
+			primaryStage.setTitle("Server");
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			Server server = new Server();
+			server.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void exit() {
+		Stage stage = (Stage) exitButton.getScene().getWindow();
+		stage.close(); // ferme la fenêtre et stoppe le serveur
+		System.exit(0);
+	}
 }
